5a3c27859da85fb1bddebe964962afdfcf3e12cb
mattjhellard <114604816+mattjhellard@users.noreply.github.com>
2022-10-25 11:35:54 -0400

    commented NextPrime
    other methods not commented as they are nearly identical to provided open hash table code (i.e, the rest isn't really my code).

--- Assignment2A.cs
+++ Assignment2A.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * How to run in VS:
  * create new Console App (.NET Frameword)
  * clear out code from new .cs file
@@ -51,25 +51,26 @@ public class HashTable <TKey, TValue> : IHashTable<TKey,TValue>
             numItems = 0;
         }
 
-        private int NextPrime(int current)
+        private int NextPrime(int current) //returns the next prime based on argument (can return argument if it's prime)
         {
-            while (current<int.MaxValue)
+            while (current<int.MaxValue) //loops until prime is found or int limit is reached
             {
-                int i;
-                for(i=2; i*i<current; i++)
+                int i; //declared outside for post-loop use
+                bool exit = false; //for loop exit condition
+                for(i=2; i*i<=current && exit==false; i++) //checks all values of i until proven not prime
                 {
-                    if (current % i == 0)
+                    if (current % i == 0) //if i leaves 0 remainder after dividing current (i cannot ==1 or ==current)
                     {
-                        i = current;
+                        exit = true; //exits loop on next condition check
                     }
                 }
-                if (i < current)
+                if (exit==false) //if for loop was exited and exit==false, then no number from 2 to sqrt(current) was found to divide with a remainder of 0, i.e, current is prime
                 {
                     return current;
                 }
-                current++;
+                current++; //if here, current number was not prime (see above comment), raise current for next loop
             }
-            return -1;
+            return -1; //failure code, due to behavior of parent class, initial argument and thus return should never be negative, only gets here if main loop exited without returning 
         }
 
         private void ReHash()
